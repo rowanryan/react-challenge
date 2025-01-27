b5e0de80edac443c52a81f61159e8a2f9b8a56f8
Jonathan van de Groep <jrr.vdgroep@gmail.com>
2025-01-27 11:00:58 +0100

    formatting

--- src/common/layouts/CoreLayout.js
+++ src/common/layouts/CoreLayout.js
@@ -1,21 +1,19 @@
-import React from 'react';
-import Header from '../components/Header';
-import SideBar from '../components/SideBar';
-import Player from '../components/Player';
+import React from "react";
+import Header from "../components/Header";
+import SideBar from "../components/SideBar";
+import Player from "../components/Player";
 
-function CoreLayout({ children , history }) {
-  return (
-    <div className="main">
-      <SideBar />
-      <div className="main__content">
-        <Header history={history} />
-        <div className="main__content__child">
-          {children}
+function CoreLayout({ children, history }) {
+    return (
+        <div className="main">
+            <SideBar />
+            <div className="main__content">
+                <Header history={history} />
+                <div className="main__content__child">{children}</div>
+            </div>
+            <Player />
         </div>
-      </div>
-      <Player />
-    </div>
-  );
+    );
 }
 
 export default CoreLayout;
